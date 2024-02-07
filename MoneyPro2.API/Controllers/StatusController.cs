fc037d5dd0192b40ff5ca197237dc194f81260b9
Elcio Reis <elcio.reis@gmail.com>
2024-02-07 16:18:45 -0300

    Inclusao de classe de Status

---
+++ MoneyPro2.API/Controllers/StatusController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoneyPro2.API.Data;
+using MoneyPro2.API.ViewModels;
+using MoneyPro2.API.ViewModels.Status;
+
+namespace MoneyPro2.API.Controllers;
+
+public class StatusController : Controller
+{
+    [HttpPost("v1/status/")]
+    public async Task<IActionResult> Status([FromServices] MoneyPro2DataContext context)
+    {
+        var status = new StatusViewModel("API em execução");
+        return Ok(new ResultViewModel<StatusViewModel>(status));
+    }
+}
---
+++ MoneyPro2.API/ViewModels/Status/StatusViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MoneyPro2.API.ViewModels.Status;
+
+public class StatusViewModel
+{
+    public StatusViewModel(string status)
+    {
+        Status = status;
+    }
+
+    public string Status { get; set; } = string.Empty;
+}
+
